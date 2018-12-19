b865ffee7446a568f5c14385496be0ffe0b5c3ed
Jaisel Rahman <jaiselrahman@gmail.com>
2018-12-19 20:21:03 +0530

    Add typing title carousel

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum;
+    var fullTxt = this.toRotate[i];
+    
+    if(i == this.toRotate.length - 1) {
+      this.isDeleting = false;
+    }
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = this.txt;
+  
+    var that = this;
+    var delta = 70;
+  
+    if (this.isDeleting) { delta /= 1.75; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 70;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
